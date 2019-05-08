3be84a93cbed2e58fe7fe5de1b3ed9025c185e9f
DESKTOP-1BJMRNO\Iker-PC <iker.nunrod@gmail.com>
2019-05-08 15:25:00 +0200

    Scrolls

--- app/src/main/java/com/interfacesgraficas/activities/MainActivity.java
+++ app/src/main/java/com/interfacesgraficas/activities/MainActivity.java
@@ -1,19 +1,32 @@
 package com.interfacesgraficas.activities;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.interfacesgraficas.R;
 
 
+
 public class MainActivity extends AppCompatActivity
 {
+    private Context context;
     private Toolbar mTopToolbar;
+    private SearchView mSearchView;
+    private MenuItem searchItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,25 +34,43 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        context = this;
+
         mTopToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(mTopToolbar);
+
+        //Cerrar buscador
+        mSearchView = (SearchView) findViewById(R.id.app_bar_search);
+
+        ScrollView layout = findViewById(R.id.ScrollViewVertical);
+        layout.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View view, MotionEvent ev)
+            {
+                InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                in.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                mSearchView = (SearchView) findViewById(R.id.app_bar_search);
+                mSearchView.setIconified(true);
+                mSearchView.clearFocus();
+                searchItem.collapseActionView();
+                return false;
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        searchItem = menu.findItem(R.id.app_bar_search);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.app_bar_search) {
             Toast.makeText(MainActivity.this, "Action clicked", Toast.LENGTH_LONG).show();
             return true;
